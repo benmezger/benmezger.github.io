b478c0ea8cf45aed7d1679ac0340751d5866a9a2
Ben Mezger <me@benmezger.nl>
2020-06-06 03:26:18 -0300

    Automatic site rebuild of Sat Jun  6 03:26:18 -03 2020
      This is a rebuild of commit '6df467d885385b7b1b74f8f4413a8f1d0bfc80d2'
      Respository: github.com/benmezger/blog

---
+++ js/pt.preview.min.8414f4eba84bf6e577b10aa83e375f8f4b4bad69e101012f8fe6fd73a4a70dda.js
@@ -0,0 +1,4 @@
+function createTippyObject(url,opts){let tippyOptions={allowHTML:true,theme:"light",interactive:true,interactiveBorder:10,delay:100,touch:"none",maxWidth:"none",inlinePositioning:false,placement:"right",};tip=tippy(url,Object.assign({},tippyOptions,{content:'<iframe width="400px" height="300px" src="'+
+url.href+
+'preview.html"></iframe>',},opts));console.log(tip);}
+$("document").ready(function(){$.each($("#backlinks~ul>li>a"),function(k,v){console.log(k,v);createTippyObject(v,{});});});
